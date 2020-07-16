1027935d0ac875a56714897434d659a5a617e483
Muhammad Faisal <muhammadfaizal480@gmail.com>
2020-07-16 10:18:04 +0500

    Implemented push in priority queue

--- algoImplementation.js
+++ algoImplementation.js
@@ -1,8 +1,8 @@
 class node
 {
-   contructor(freq = 0,letter = null)
+   constructor(frequency,letter)
    {
-       this.freq = freq;
+       this.freq = frequency;
        this.letter = letter;
        this.next = null;
    } 
@@ -16,10 +16,11 @@ class priority_queue
     }
     push(node1)
     {
-        if(head == null)
+        if(this.head == null)
         {
-            head = node1;
+           this.head = node1;
             node1.next = null;
+            this.tail = node1;
             this.head.next = null;
         }
         else if(this.head.freq < node1.freq)
@@ -32,6 +33,18 @@ class priority_queue
             this.tail.next = node1;
             this.tail = node1;
             this.tail.next = null;
+        }
+        else
+        {
+            var bptr,rptr = null;
+            rptr = bptr = this.head;
+            while(rptr.freq >= node1.freq)
+            {
+                bptr = rptr;
+                rptr = rptr.next;
+            }
+            node1.next = rptr;
+            bptr.next =  node1;
         }
         
     }
@@ -50,6 +63,14 @@ class priority_queue
 
 console.log("Working");
 n1 = new node(12,'e');
+n2 = new node(15,'a');
+n3 = new node(3,'y');
+n4 = new node(18,'v');
+n5 = new node(6,'w');
 newQueue = new priority_queue();
 newQueue.push(n1);
+newQueue.push(n2);
+newQueue.push(n3);
+newQueue.push(n4);
+newQueue.push(n5);
 newQueue.print();
