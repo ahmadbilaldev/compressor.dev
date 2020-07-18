66c0b5d7de7d89437e1e9d815e431df22c5cad6e
Muhammad Faisal <muhammadfaizal480@gmail.com>
2020-07-18 21:17:51 +0500

    Implemented lzw decoder

--- src/lzw.js
+++ src/lzw.js
@@ -21,7 +21,7 @@ function lzwEncoder(inputString)
     var encodedData = [];
     var currentChar;
     var previousChar = convertedData[0];
-    var comparisonCode = 256; 
+    var charCode = 256; 
     for (var i = 1; i < convertedData.length; i++) 
     {
         currentChar=convertedData[i];
@@ -34,8 +34,8 @@ function lzwEncoder(inputString)
             if(previousChar.length > 1) encodedData.push(myDict[previousChar]);
             else encodedData.push(previousChar.charCodeAt(0));
            
-            myDict[previousChar + currentChar] = comparisonCode;
-            comparisonCode++;
+            myDict[previousChar + currentChar] = charCode;
+            charCode++;
             previousChar = currentChar;
         }
     }
@@ -44,7 +44,7 @@ function lzwEncoder(inputString)
     else encodedData.push(previousChar.charCodeAt(0));
    
     /*
-    converting ascii code into symbols
+    converting ascii charCode into symbols
     */
     for (var i=0; i<encodedData.length; i++) {
         encodedData[i] = String.fromCharCode(encodedData[i]);
@@ -52,10 +52,54 @@ function lzwEncoder(inputString)
     return encodedData.join("");
 }
 
+function lzw_decode(encodedString) {
+    var newDict = {};
+    var convData = (encodedString + "").split("");
+    var currChar = convData[0];
+    var prevSelectedChar = currChar;
+    var decodedString = [currChar]; //selecting the first element as output because 1st element is always less than 256
+    var charCode = 256;
+    var selectedChar;
+    for (var i=1; i<convData.length; i++) 
+    {
+        var currCode = convData[i].charCodeAt(0);
+        if (currCode < 256) 
+        {
+         selectedChar = convData[i];
+        }
+        else 
+        {
+            //if it is present
+            if(newDict[currCode] != null)
+            {
+                selectedChar = newDict[currCode];
+            }
+            else
+            {
+                selectedChar = (prevSelectedChar + currChar); //if its not present then its always going to be last+current
+            }
+        }
 
+        decodedString.push(selectedChar);
+        currChar = selectedChar.charAt(0);
+        newDict[charCode] = prevSelectedChar + currChar;
+        charCode++;
+        prevSelectedChar = selectedChar;
+    }
+    return decodedString.join("");
+
+}
 
 
 let string = "abcaababcbbcab";
+console.log("the main string is",string);
 let encoded = [1,2,3,1];
 let newObj = getFrequencies(string);
-console.log(lzwEncoder(string));
+encodedStr = lzwEncoder(string);
+console.log("The encode string is",encodedStr);
+decodedStr = lzw_decode(encodedStr);
+console.log("The decoded string is",decodedStr);
+if(string == decodedStr)
+{
+    console.log("its working")
+}
