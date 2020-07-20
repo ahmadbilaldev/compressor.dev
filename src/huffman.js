4e774839544ef01292ebafca073eb2a96fa1ddd4
Ahmad Bilal <ahmadbilaldev@gmail.com>
2020-07-20 16:23:55 +0500

    Decode encoded string

--- src/huffman.js
+++ src/huffman.js
@@ -159,3 +159,87 @@ function huffmanEncode(inputString) {
 	let byteArray = codesToByteArray(inputString, codes, zeroPadding);
 	outputEncodedString(codes, byteArray, zeroPadding);
 }
+
+/**
+ * Complete decoding funciton.
+ * @param {*} fileName
+ */
+
+function huffmanDecode(fileName) {
+	const fileData = fs.readFileSync(fileName);
+	let fileLength = fileData.length;
+
+	let fileString = fileData;
+	let byteString = new Uint8Array(fileLength);
+
+	for (i = 0; i < fileData.length; i++) {
+		byteString[i] = fileString[i];
+	}
+
+	let codingSchemeLength = byteArrayToInt(byteString.subarray(0, 4));
+	let zeroPadding = byteString[4]; // A count of zero padding applied.
+	// Array of char codes of coding scheme
+	let codingScheme = byteString.subarray(5, 5 + codingSchemeLength);
+
+	// extract coding scheme as a string
+	let codingSchemeString = '';
+	for (i = 0; i < codingSchemeLength; i++) {
+		codingSchemeString += String.fromCharCode(codingScheme[i]);
+	}
+
+	// Convert coding scheme string to json to help converting into object.
+	codingSchemeString = '{"' + codingSchemeString + '"}';
+	codingSchemeString = codingSchemeString.replace(/,/g, '","');
+	codingSchemeString = codingSchemeString.replace(/:/g, '":"');
+	codingSchemeString = codingSchemeString.replace('"",""', '","');
+	codingSchemeString = codingSchemeString.replace('"\\"', '"\\""');
+
+	// Now, convert json into object.
+	let codingSchemeObject = JSON.parse(codingSchemeString);
+
+	// store the actual encoded message after the coding scheme.
+	let encodedString = byteString.subarray(5 + codingSchemeLength);
+
+	// Convert to bits for decoding, padding left is required in JavaScript
+	let encodedBits = '';
+	for (i = 0; i < encodedString.length; i++) {
+		encodedBits += encodedString[i].toString(2).paddingLeft('00000000');
+	}
+
+	// Deletes the zero padding (we applied it while encoding to help convert into bits)
+	encodedBits = encodedBits.substring(0, encodedBits.length - zeroPadding);
+
+	// Swap the key and value of the object. Values become keys, keys become values.
+	codingSchemeObject = flipObject(codingSchemeObject);
+
+	// Finally decode the string using the coding scheme object.
+	let decodedString = '';
+	let buffer = '';
+
+	for (i = 0; i < encodedBits.length; i++) {
+		buffer += encodedBits.charAt(i);
+
+		if (codingSchemeObject[buffer] !== undefined) {
+			decodedString += codingSchemeObject[buffer];
+			buffer = '';
+		} else {
+			console.log('ERROR\n');
+		}
+	}
+	return decodedString;
+}
+
+// ************ DEBUGGING **********
+
+// let inputString = 'aabbccccc';
+// let fr = getFrequencies(inputString);
+// let rootNode = buildHuffmanTree(fr);
+// let codes = {}; // object with key-value pairs.
+// huffmanCodes(rootNode, codes, '');
+// console.log(codes);
+// let zeroPadding = { count: 0 };
+// let byteArray = codesToByteArray(inputString, codes, zeroPadding);
+// outputEncodedString(codes, byteArray, zeroPadding);
+
+let file = 'encoded.txt';
+huffmanDecode(file);
