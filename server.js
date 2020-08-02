47ddfabf44d3d2da029d210e06260d05732bd9a6
Ahmad Bilal <ahmadbilaldev@gmail.com>
2020-08-02 06:26:39 +0500

    Fix route errors

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const huff = require('./src/huffman');
+
+const huff = require('./utils/huffman');
 
 // Initiate a instance of express.
 const app = express();
 const PORT = process.env.port || 3000;
 
-// Make public the root.
+// Make ./public the web root.
 app.use(express.static('public'));
 
 // Provide libraries through node modules.
-app.use('/scripts', express.static(`${__dirname}/node_modules/`));
+// app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 
 // Start
-app.listen(PORT, () => console.log(`Server running on ${PORT}....`));
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}....`));
 
 // Get index.html
 app.get('/', function (req, res) {
@@ -22,14 +23,13 @@ app.get('/', function (req, res) {
 
 app.use(
 	bodyParser.urlencoded({
-		limit: '100mb',
+		limit: '20mb',
 		extended: true,
 		parameterLimit: 50000,
 	})
 );
 
 // DECODE
-
 app.post('/decode', function (req, res) {
 	let encodedBits = req.body.bits;
 	let codingSchemeObject = req.body.obj;
@@ -40,18 +40,29 @@ app.post('/decode', function (req, res) {
 
 app.use(
 	bodyParser.urlencoded({
-		limit: '100mb',
+		limit: '20mb',
 		extended: true,
 		parameterLimit: 50000,
 	})
 );
 
 // ENCODE
-
 app.post('/encode', function (req, res) {
 	let inputString = req.body.string;
 	console.log(inputString);
-	let encodedObj = huff.huffmanEncode(inputString);
-	console.log(encodedObj);
-	res.send(encodedObj);
+
+	let encodedObject = huff.huffmanEncode(inputString);
+	console.log(encodedObject);
+	res.json(encodedObject);
+	// let finalEncodedArray = huff.huffmanEncode(inputString);
+	// console.log(finalEncodedArray);
+	// const outFile = `encodedTest.txt`;
+	// const outData = finalEncodedArray;
+	// fs.writeFile(outFile, outData, function (err) {
+	// 	if (err) {
+	// 		console.log(error);
+	// 	}
+	// });
+	// console.log(`${__dirname}/encodedTest.txt`);
+	// res.download(`${__dirname}/encodedTest.txt`);
 });
