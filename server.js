51c43c26765fbe330df80ae9df3287d7beec20ca
Ahmad Bilal <ahmadbilaldev@gmail.com>
2020-08-16 15:30:04 +0500

    Fix typo

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const lzw = require('./utils/lzw');
 
 // Initiate a instance of express.
 const app = express();
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 // Make ./public the web root.
 app.use(express.static('public'));
